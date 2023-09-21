6f7a31c43c6fe917087c39098afa5bca2ab311ae
ThunsOJ <115113975+ThunsOJ@users.noreply.github.com>
2023-09-21 09:11:41 +0200

    ye

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Addition addition = new Addition();
-        System.out.println(addition.calculate(2,1));
+        System.out.println(addition.calculate(2,4));
     }
 }
